3d92a0e4b57b1f62c06066d19f606d488bd7012c
EricRootLee <erickiokom@gmail.com>
2020-12-31 18:34:30 +0300

    FIxed some issues

--- app/src/main/java/com/skylabstechke/todo/fragments/list/ListFragment.kt
+++ app/src/main/java/com/skylabstechke/todo/fragments/list/ListFragment.kt
@@ -1,19 +1,30 @@
 package com.skylabstechke.todo.fragments.list
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.skylabstechke.todo.R
+
 
 
 class ListFragment : Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View ?{
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false)
+        val view = inflater.inflate(R.layout.fragment_list, container, false)
+
+        view.findViewById<FloatingActionButton>(R.id.floatingActionButton).setOnClickListener {
+            findNavController().navigate(R.id.action_listFragment_to_addFragment)
+        }
+        return view
+
     }
 
 }
