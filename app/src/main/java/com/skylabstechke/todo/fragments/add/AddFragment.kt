eda4ac44b1f0f5548f428904d6020ad676cd843b
EricRootLee <erickiokom@gmail.com>
2021-01-01 13:23:01 +0300

    Linked add option menu

--- app/src/main/java/com/skylabstechke/todo/fragments/add/AddFragment.kt
+++ app/src/main/java/com/skylabstechke/todo/fragments/add/AddFragment.kt
@@ -1,18 +1,24 @@
 package com.skylabstechke.todo.fragments.add
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.skylabstechke.todo.R
 
 class AddFragment : Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         // Inflate the layout for this fragment
+        setHasOptionsMenu(true)
         return inflater.inflate(R.layout.fragment_add, container, false)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.add_fragment_menu, menu)
     }
 
 
