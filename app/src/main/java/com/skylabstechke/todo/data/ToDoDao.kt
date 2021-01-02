6b3576d2fe7067e1cc9977cc1a4febc5ce311626
EricRootLee <erickiokom@gmail.com>
2021-01-02 23:31:39 +0300

    Select Query

--- app/src/main/java/com/skylabstechke/todo/data/ToDoDao.kt
+++ app/src/main/java/com/skylabstechke/todo/data/ToDoDao.kt
@@ -1,4 +1,11 @@
 package com.skylabstechke.todo.data
 
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Query
+
+@Dao
 interface ToDoDao {
+    @Query("SELECT * FROM todo_table ORDER BY id ASC")
+     fun getAllData():LiveData<List<ToDoData>>
 }
