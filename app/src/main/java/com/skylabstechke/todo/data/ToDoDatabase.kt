a684d394d6b28b38628a76397efa82cfa1e107cc
EricRootLee <erickiokom@gmail.com>
2021-01-02 23:51:11 +0300

    Pic from there

--- app/src/main/java/com/skylabstechke/todo/data/ToDoDatabase.kt
+++ app/src/main/java/com/skylabstechke/todo/data/ToDoDatabase.kt
@@ -5,4 +5,9 @@ import androidx.room.RoomDatabase
 @Database(entities = [ToDoData::class],version = 1,exportSchema = false)
 abstract class ToDoDatabase : RoomDatabase() {
     abstract fun toDoDao():ToDoDao
+    companion object{
+        @Volatile
+        private var INSTANCE :ToDoDatabase ? = null
+
+    }
 }
