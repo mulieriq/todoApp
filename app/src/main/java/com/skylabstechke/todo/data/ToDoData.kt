c8ac113396f9a0b7a7aad89f4443b20bb1f83bd3
EricRootLee <erickiokom@gmail.com>
2021-01-02 23:24:14 +0300

    Auto generate primary key

--- app/src/main/java/com/skylabstechke/todo/data/ToDoData.kt
+++ app/src/main/java/com/skylabstechke/todo/data/ToDoData.kt
@@ -1,10 +1,12 @@
 package com.skylabstechke.todo.data
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 
 @Entity(tableName = "todo_table")
 data class ToDoData(
+    @PrimaryKey(autoGenerate = true)
     var id:Int,
     var title:String,
     var priority: Priority,
