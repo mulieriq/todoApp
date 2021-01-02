ddaec4a908d276fe05cc670f5443b4312bccefe9
EricRootLee <erickiokom@gmail.com>
2021-01-02 23:32:43 +0300

    INsert data

--- app/src/main/java/com/skylabstechke/todo/data/ToDoDao.kt
+++ app/src/main/java/com/skylabstechke/todo/data/ToDoDao.kt
@@ -2,10 +2,13 @@ package com.skylabstechke.todo.data
 
 import androidx.lifecycle.LiveData
 import androidx.room.Dao
+import androidx.room.Insert
 import androidx.room.Query
 
 @Dao
 interface ToDoDao {
     @Query("SELECT * FROM todo_table ORDER BY id ASC")
-     fun getAllData():LiveData<List<ToDoData>>
+    fun getAllData():LiveData<List<ToDoData>>
+    @Insert
+    fun insertData(toDoData: ToDoData)
 }
