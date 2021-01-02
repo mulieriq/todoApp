16bb59d21851c1e3cb3dbe98f65e0c014eb591ec
EricRootLee <erickiokom@gmail.com>
2021-01-02 23:34:38 +0300

    Onconflict flag

--- app/src/main/java/com/skylabstechke/todo/data/ToDoDao.kt
+++ app/src/main/java/com/skylabstechke/todo/data/ToDoDao.kt
@@ -3,12 +3,14 @@ package com.skylabstechke.todo.data
 import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 
 @Dao
 interface ToDoDao {
     @Query("SELECT * FROM todo_table ORDER BY id ASC")
-    fun getAllData():LiveData<List<ToDoData>>
-    @Insert
-    fun insertData(toDoData: ToDoData)
+    fun getAllData(): LiveData<List<ToDoData>>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertData(toDoData: ToDoData)
 }
