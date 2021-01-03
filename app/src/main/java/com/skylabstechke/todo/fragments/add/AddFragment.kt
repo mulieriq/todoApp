a6c83d322f605f6426c95e57efe69684165a97c6
EricRootLee <erickiokom@gmail.com>
2021-01-03 20:58:06 +0300

    Adding data to the db

--- app/src/main/java/com/skylabstechke/todo/fragments/add/AddFragment.kt
+++ app/src/main/java/com/skylabstechke/todo/fragments/add/AddFragment.kt
@@ -3,13 +3,19 @@ package com.skylabstechke.todo.fragments.add
 import android.os.Bundle
 import android.text.TextUtils
 import android.view.*
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.skylabstechke.todo.R
 import com.skylabstechke.todo.data.model.Priority
 import com.skylabstechke.todo.data.model.ToDoData
+import com.skylabstechke.todo.data.viewmodel.ToDoViewModel
 import kotlinx.android.synthetic.main.fragment_add.*
 
 class AddFragment : Fragment() {
+
+    private val mToDoViewModel :ToDoViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -42,8 +48,13 @@ class AddFragment : Fragment() {
             val newData = ToDoData(
                 0,
                 mTitle,
+                parsePriority(mPriority),
+                mDescription
+            )
+            mToDoViewModel.insertData(newData)
 
-                )
+            Toast.makeText(requireContext(),"Successfully added!",Toast.LENGTH_LONG).show()
+            findNavController().navigate(R.id.action_addFragment_to_listFragment)
         }
 
     }
