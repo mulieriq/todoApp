6b09e417626c22b5f6fb9c1081fcc283e7929976
EricRootLee <erickiokom@gmail.com>
2021-01-03 18:26:43 +0300

    Created viewmodel

---
+++ app/src/main/java/com/skylabstechke/todo/data/ToDoViewModel.kt
@@ -0,0 +1,7 @@
+package com.skylabstechke.todo.data
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class ToDoViewModel(application: Application) : AndroidViewModel(application) {
+}
