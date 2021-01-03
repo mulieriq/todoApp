f5ec0047deb2da9d8ff82a5161b65281a63c8725
EricRootLee <erickiokom@gmail.com>
2021-01-03 18:42:19 +0300

    View model geeting data

--- app/src/main/java/com/skylabstechke/todo/data/ToDoViewModel.kt
+++ app/src/main/java/com/skylabstechke/todo/data/ToDoViewModel.kt
@@ -2,15 +2,19 @@ package com.skylabstechke.todo.data
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
 import com.skylabstechke.todo.data.databaseclient.ToDoDatabase
+import com.skylabstechke.todo.data.model.ToDoData
 import com.skylabstechke.todo.data.repository.ToDoRepository
 
 class ToDoViewModel(application: Application) : AndroidViewModel(application) {
 
-    private val toDoDao= ToDoDatabase.getDatabase(application).toDoDao()
-    private val repository:ToDoRepository
+    private val toDoDao = ToDoDatabase.getDatabase(application).toDoDao()
+    private val repository: ToDoRepository
+    private lateinit var getAllData: LiveData<List<ToDoData>>
 
     init {
         repository = ToDoRepository(toDoDao)
+        getAllData = repository.getAllData
     }
 }
