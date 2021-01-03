60be654ba52db8292bf4f276a9ed169f1b0a8eee
EricRootLee <erickiokom@gmail.com>
2021-01-03 20:52:54 +0300

    Parsing priority

--- app/src/main/java/com/skylabstechke/todo/fragments/add/AddFragment.kt
+++ app/src/main/java/com/skylabstechke/todo/fragments/add/AddFragment.kt
@@ -5,6 +5,8 @@ import android.text.TextUtils
 import android.view.*
 import androidx.fragment.app.Fragment
 import com.skylabstechke.todo.R
+import com.skylabstechke.todo.data.model.Priority
+import com.skylabstechke.todo.data.model.ToDoData
 import kotlinx.android.synthetic.main.fragment_add.*
 
 class AddFragment : Fragment() {
@@ -34,11 +36,38 @@ class AddFragment : Fragment() {
         val mTitle = title_et.text.toString()
         val mPriority = priorities_spinner.selectedItem.toString()
         val mDescription = descriptions_et.text.toString()
+
+        val validation = verifyDataFromUser(mTitle, mDescription)
+        if (validation) {
+            val newData = ToDoData(
+                0,
+                mTitle,
+
+                )
+        }
+
     }
 
     private fun verifyDataFromUser(title: String, description: String): Boolean {
         return if (TextUtils.isEmpty(title) || TextUtils.isEmpty(description)) {
             false
         } else !(title.isEmpty() || description.isEmpty())
+    }
+
+    private fun parsePriority(priority: String): Priority {
+
+        return when (priority) {
+            "High Priority" -> {
+                Priority.HIGH
+            }
+            "Medium Priority" -> {
+                Priority.MEDIUM
+            }
+            "Low Priority" -> {
+                Priority.LOW
+            }
+            else -> Priority.LOW
+        }
+
     }
 }
