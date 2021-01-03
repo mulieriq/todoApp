dd7d7c6c16449700d522b6c9075e40d09edfff4c
EricRootLee <erickiokom@gmail.com>
2021-01-03 17:59:16 +0300

    From priority

--- app/src/main/java/com/skylabstechke/todo/data/Converter.kt
+++ app/src/main/java/com/skylabstechke/todo/data/Converter.kt
@@ -1,4 +1,13 @@
 package com.skylabstechke.todo.data
 
+import androidx.room.TypeConverter
+
 class Converter {
+
+    @TypeConverter
+    fun  fromPriority(priority: Priority):String{
+        return priority.name
+    }
+
+
 }
