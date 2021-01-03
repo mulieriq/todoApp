a18b0da213ab8efcb942fa78201ec98296d5fbfe
EricRootLee <erickiokom@gmail.com>
2021-01-03 18:40:14 +0300

    Init  Respository

--- app/src/main/java/com/skylabstechke/todo/data/ToDoViewModel.kt
+++ app/src/main/java/com/skylabstechke/todo/data/ToDoViewModel.kt
@@ -2,6 +2,15 @@ package com.skylabstechke.todo.data
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import com.skylabstechke.todo.data.databaseclient.ToDoDatabase
+import com.skylabstechke.todo.data.repository.ToDoRepository
 
 class ToDoViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val toDoDao= ToDoDatabase.getDatabase(application).toDoDao()
+    private val repository:ToDoRepository
+
+    init {
+        repository = ToDoRepository(toDoDao)
+    }
 }
