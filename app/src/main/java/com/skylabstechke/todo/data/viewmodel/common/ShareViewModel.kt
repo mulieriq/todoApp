42890d3aa045d0aa146f05380cbc448b705c54dd
EricRootLee <erickiokom@gmail.com>
2021-01-03 21:11:05 +0300

    Common view model

---
+++ app/src/main/java/com/skylabstechke/todo/data/viewmodel/common/ShareViewModel.kt
@@ -0,0 +1,7 @@
+package com.skylabstechke.todo.data.viewmodel.common
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class ShareViewModel (application: Application):AndroidViewModel(application){
+}
