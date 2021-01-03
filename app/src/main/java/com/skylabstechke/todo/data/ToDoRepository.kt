36bc55240460deff46fa89c708a7ea803870561c
EricRootLee <erickiokom@gmail.com>
2021-01-03 18:09:37 +0300

    Clean code

--- app/src/main/java/com/skylabstechke/todo/data/ToDoRepository.kt
+++ app/src/main/java/com/skylabstechke/todo/data/ToDoRepository.kt
@@ -2,16 +2,9 @@ package com.skylabstechke.todo.data
 
 import androidx.lifecycle.LiveData
 
-class ToDoRepository (private val toDoDao: ToDoDao){
-
-
-     val getAllData:LiveData<List<ToDoData>> = toDoDao.getAllData()
-
-
-    suspend fun insertData(toDoData: ToDoData){
+class ToDoRepository(private val toDoDao: ToDoDao) {
+    val getAllData: LiveData<List<ToDoData>> = toDoDao.getAllData()
+    suspend fun insertData(toDoData: ToDoData) {
         toDoDao.insertData(toDoData)
     }
-
-
-
 }
