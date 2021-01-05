401523af74c65dde9a16af5e50f278308a671db7
EricRootLee <erickiokom@gmail.com>
2021-01-05 14:20:37 +0300

    Adapter ready

--- app/src/main/java/com/skylabstechke/todo/data/viewmodel/ToDoViewModel.kt
+++ app/src/main/java/com/skylabstechke/todo/data/viewmodel/ToDoViewModel.kt
@@ -3,9 +3,12 @@ package com.skylabstechke.todo.data.viewmodel
 import android.app.Application
 import android.view.View
 import android.widget.AdapterView
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.viewModelScope
+import com.skylabstechke.todo.R
 import com.skylabstechke.todo.data.databaseclient.ToDoDatabase
 import com.skylabstechke.todo.data.model.ToDoData
 import com.skylabstechke.todo.data.repository.ToDoRepository
@@ -16,14 +19,32 @@ class ToDoViewModel(application: Application) : AndroidViewModel(application) {
 
 
     val listener: AdapterView.OnItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-        override fun onItemSelected(p0: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
             when
                     (position) {
                 0 -> {
+                    (parent?.getChildAt(0) as TextView).setTextColor(
+                        ContextCompat.getColor(
+                            application,
+                            R.color.red
+                        )
+                    )
                 }
                 1 -> {
+                    (parent?.getChildAt(1) as TextView).setTextColor(
+                        ContextCompat.getColor(
+                            application,
+                            R.color.yellow
+                        )
+                    )
                 }
                 2 -> {
+                    (parent?.getChildAt(2) as TextView).setTextColor(
+                        ContextCompat.getColor(
+                            application,
+                            R.color.green
+                        )
+                    )
                 }
             }
         }
