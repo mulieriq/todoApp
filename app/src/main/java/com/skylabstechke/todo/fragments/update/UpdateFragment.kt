8de2e1ebfc30954965b956a23f79277e9a30c4e2
EricRootLee <erickiokom@gmail.com>
2021-01-05 22:18:24 +0300

    Parse priority

--- app/src/main/java/com/skylabstechke/todo/fragments/update/UpdateFragment.kt
+++ app/src/main/java/com/skylabstechke/todo/fragments/update/UpdateFragment.kt
@@ -5,6 +5,9 @@ import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import com.skylabstechke.todo.R
+import com.skylabstechke.todo.data.model.Priority
+import kotlinx.android.synthetic.main.fragment_add.view.*
+import kotlinx.android.synthetic.main.fragment_update.view.*
 
 
 class UpdateFragment : Fragment() {
@@ -17,7 +20,19 @@ class UpdateFragment : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_update, container, false)
         setHasOptionsMenu(true)
+
+        view.current_title_et.setText(args.current.title)
+        view.current_descriptions_et.setText(args.current.description)
+        view.priorities_spinner.setSelection(parsePriotity(args.current.priority))
         return view
+    }
+
+    private fun parsePriotity(priority: Priority): Int {
+        return when (priority) {
+            Priority.HIGH -> 0
+            Priority.MEDIUM -> 1
+            Priority.LOW -> 2
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
