dec210db1364b4519ec7770ca36800ad95cda379
EricRootLee <erickiokom@gmail.com>
2021-01-05 22:35:24 +0300

    Updating data

--- app/src/main/java/com/skylabstechke/todo/fragments/update/UpdateFragment.kt
+++ app/src/main/java/com/skylabstechke/todo/fragments/update/UpdateFragment.kt
@@ -2,13 +2,17 @@ package com.skylabstechke.todo.fragments.update
 
 import android.os.Bundle
 import android.view.*
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
 import com.skylabstechke.todo.R
 import com.skylabstechke.todo.data.model.Priority
+import com.skylabstechke.todo.data.model.ToDoData
 import com.skylabstechke.todo.data.viewmodel.common.ShareViewModel
+import kotlinx.android.synthetic.main.fragment_add.*
 import kotlinx.android.synthetic.main.fragment_add.view.*
+import kotlinx.android.synthetic.main.fragment_update.*
 import kotlinx.android.synthetic.main.fragment_update.view.*
 
 
@@ -34,11 +38,25 @@ class UpdateFragment : Fragment() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
 
         if(item.itemId == R.id.update_save){
-            update
+            updateItem()
         }
         return super.onOptionsItemSelected(item)
 
 
+    }
+
+    private fun updateItem() {
+        val title = current_title_et.text.toString()
+        val description = current_descriptions_et.text.toString()
+        val priority = priorities_spinner.selectedItem.toString()
+        val validation = mShareViewModel.verifyDataFromUser(title,description)
+        if(validation){
+            val updateDate =  ToDoData(
+
+            )
+        }else{
+            Toast.makeText(this,"Please fill out all fields",Toast.LENGTH_LONG).show()
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
