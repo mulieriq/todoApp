462b87ba39ddc417fb5767a30e9e604c9918b9db
EricRootLee <erickiokom@gmail.com>
2021-01-06 14:22:37 +0300

    Custom attribute

--- app/src/main/java/com/skylabstechke/todo/fragments/BindingAdapters.kt
+++ app/src/main/java/com/skylabstechke/todo/fragments/BindingAdapters.kt
@@ -1,13 +1,14 @@
 package com.skylabstechke.todo.fragments
 
+import androidx.databinding.BindingAdapter
 import androidx.navigation.findNavController
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.skylabstechke.todo.R
 
 class BindingAdapters {
-
-
     companion object {
+        @BindingAdapter("android:navigateToAddFragment")
+        @JvmStatic
         fun navigateToAddFragment(view: FloatingActionButton, navigate: Boolean) {
             view.setOnClickListener {
                 if (navigate) {
