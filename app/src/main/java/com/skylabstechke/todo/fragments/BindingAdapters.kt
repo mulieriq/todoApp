ea5f83708f5a8d90412948642d944bb991e852b2
EricRootLee <erickiokom@gmail.com>
2021-01-06 14:14:55 +0300

    Added a binding adapter

---
+++ app/src/main/java/com/skylabstechke/todo/fragments/BindingAdapters.kt
@@ -0,0 +1,19 @@
+package com.skylabstechke.todo.fragments
+
+import androidx.navigation.findNavController
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.skylabstechke.todo.R
+
+class BindingAdapters {
+
+
+    companion object {
+        fun navigateToAddFragment(view: FloatingActionButton, navigate: Boolean) {
+            view.setOnClickListener {
+                if (navigate) {
+                    view.findNavController().navigate(R.id.action_listFragment_to_addFragment)
+                }
+            }
+        }
+    }
+}
