0a2ecaacecc19904ffa670087f9f1ba430d9d133
EricRootLee <erickiokom@gmail.com>
2021-01-07 15:29:04 +0300

    Binding adapter ready

--- app/src/main/java/com/skylabstechke/todo/fragments/BindingAdapters.kt
+++ app/src/main/java/com/skylabstechke/todo/fragments/BindingAdapters.kt
@@ -21,7 +21,10 @@ class BindingAdapters {
         @BindingAdapter("android:emptyDatabase")
         @JvmStatic
         fun emptyDatabase(view:View,emptyDatabase:MutableLiveData<Boolean>){
-
+            when(emptyDatabase.value){
+                true -> view.visibility = View.VISIBLE
+                false -> view.visibility = View.INVISIBLE
+            }
         }
     }
 }
