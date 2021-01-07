955892ad61adc47fa4cf844f69708095912da7e9
EricRootLee <erickiokom@gmail.com>
2021-01-07 23:37:12 +0300

    sendDataToUpdateFragment

--- app/src/main/java/com/skylabstechke/todo/fragments/BindingAdapters.kt
+++ app/src/main/java/com/skylabstechke/todo/fragments/BindingAdapters.kt
@@ -3,12 +3,15 @@ package com.skylabstechke.todo.fragments
 import android.view.View
 import android.widget.Spinner
 import androidx.cardview.widget.CardView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.databinding.BindingAdapter
 import androidx.lifecycle.MutableLiveData
 import androidx.navigation.findNavController
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.skylabstechke.todo.R
 import com.skylabstechke.todo.data.model.Priority
+import com.skylabstechke.todo.data.model.ToDoData
+import com.skylabstechke.todo.fragments.list.ListFragmentDirections
 
 class BindingAdapters {
     companion object {
@@ -47,6 +50,16 @@ class BindingAdapters {
                 Priority.HIGH -> cardView.setCardBackgroundColor(cardView.context.getColor(R.color.red))
                 Priority.MEDIUM -> cardView.setCardBackgroundColor(cardView.context.getColor(R.color.yellow))
                 Priority.LOW -> cardView.setCardBackgroundColor(cardView.context.getColor(R.color.green))
+            }
+        }
+
+        @BindingAdapter("android:sendDataToUpdateFragment")
+        @JvmStatic
+
+        fun sendDataToUpdateFragment(view:ConstraintLayout,curretItem:ToDoData){
+            view.setOnClickListener {
+                val action = ListFragmentDirections.actionListFragmentToUpdateFragment(curretItem)
+                view.findNavController().navigate(action)
             }
         }
     }
