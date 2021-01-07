daeb208f1544a5181665a543fd3820af1e6a0fdc
EricRootLee <erickiokom@gmail.com>
2021-01-07 19:18:37 +0300

    Recycler view edits

--- app/src/main/java/com/skylabstechke/todo/fragments/list/ListAdapter.kt
+++ app/src/main/java/com/skylabstechke/todo/fragments/list/ListAdapter.kt
@@ -1,7 +1,6 @@
 package com.skylabstechke.todo.fragments.list
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
 import androidx.navigation.findNavController
@@ -9,24 +8,45 @@ import androidx.recyclerview.widget.RecyclerView
 import com.skylabstechke.todo.R
 import com.skylabstechke.todo.data.model.Priority
 import com.skylabstechke.todo.data.model.ToDoData
+import com.skylabstechke.todo.databinding.RowLayoutBinding
 import kotlinx.android.synthetic.main.row_layout.view.*
 
 class ListAdapter : RecyclerView.Adapter<ListAdapter.MyViewHolder>() {
 
-    var datalist = emptyList<ToDoData>()
+    private var datalist = emptyList<ToDoData>()
 
-    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+    class MyViewHolder(private val binding: RowLayoutBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(toDoData: ToDoData) {
+            binding.toDoData = toDoData
+            binding.executePendingBindings()
+
+        }
+
+        companion object {
+            fun from(parent: ViewGroup): MyViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = RowLayoutBinding.inflate(layoutInflater, parent, false)
+                return MyViewHolder(binding)
+            }
+
+        }
+
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.row_layout, parent, false)
-        return MyViewHolder(view)
+//        val view = LayoutInflater.from(parent.context).inflate(R.layout.row_layout, parent, false)
+//        return MyViewHolder(view)
+        return  MyViewHolder.from(parent)
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         holder.itemView.title_txt.text = datalist[position].title
         holder.itemView.description_txt.text = datalist[position].description
         holder.itemView.row_background.setOnClickListener {
-            val action = ListFragmentDirections.actionListFragmentToUpdateFragment(datalist[position])
+            val action =
+                ListFragmentDirections.actionListFragmentToUpdateFragment(datalist[position])
             holder.itemView.findNavController().navigate(action)
         }
         val priority = datalist[position].priority
