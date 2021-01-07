e900ca9aaba78572808cfef3626a09f0eb416041
EricRootLee <erickiokom@gmail.com>
2021-01-08 01:29:02 +0300

    Implemented Itemtouch

--- app/src/main/java/com/skylabstechke/todo/utilis/SwipeToDelete.kt
+++ app/src/main/java/com/skylabstechke/todo/utilis/SwipeToDelete.kt
@@ -1,4 +1,19 @@
 package com.skylabstechke.todo.utilis
 
-class SwipeToDelete {
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.RecyclerView
+
+abstract class SwipeToDelete : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+
+    override fun onMove(
+        recyclerView: RecyclerView,
+        viewHolder: RecyclerView.ViewHolder,
+        target: RecyclerView.ViewHolder
+    ): Boolean {
+        return false
+    }
+
+    override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+
+    }
 }
