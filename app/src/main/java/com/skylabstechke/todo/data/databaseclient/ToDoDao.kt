65ccb393e3085cd1bd6247869df5992757829f51
EricRootLee <erickiokom@gmail.com>
2021-01-08 15:30:49 +0300

    Search dao

--- app/src/main/java/com/skylabstechke/todo/data/databaseclient/ToDoDao.kt
+++ app/src/main/java/com/skylabstechke/todo/data/databaseclient/ToDoDao.kt
@@ -8,12 +8,19 @@ import com.skylabstechke.todo.data.model.ToDoData
 interface ToDoDao {
     @Query("SELECT * FROM todo_table ORDER BY id ASC")
     fun getAllData(): LiveData<List<ToDoData>>
+
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insertData(toDoData: ToDoData)
+
     @Update
     suspend fun updateData(toDoData: ToDoData)
+
     @Delete
     suspend fun deleteData(toDoData: ToDoData)
+
     @Query("DELETE  FROM todo_table")
     suspend fun deleteAll()
+
+    @Query("SELECT *   FROM todo_table WHERE title LIKE :searchQuery")
+    suspend fun searchDataBase(searchQuery: String): LiveData<List<ToDoData>>
 }
