f4774329fb2bf2e864b2839a5df7626136dd5dea
EricRootLee <erickiokom@gmail.com>
2021-01-08 14:27:13 +0300

    Diffutills done

--- app/src/main/java/com/skylabstechke/todo/utilis/ToDoDiffUtil.kt
+++ app/src/main/java/com/skylabstechke/todo/utilis/ToDoDiffUtil.kt
@@ -8,18 +8,21 @@ class ToDoDiffUtil(
     private val newList: List<ToDoData>
 ) : DiffUtil.Callback() {
     override fun getOldListSize(): Int {
-        TODO("Not yet implemented")
+        return oldList.size
     }
 
     override fun getNewListSize(): Int {
-        TODO("Not yet implemented")
+        return newList.size
     }
 
     override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
-        TODO("Not yet implemented")
+        return oldList[oldItemPosition] === newList[newItemPosition]
     }
 
     override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
-        TODO("Not yet implemented")
+        return oldList[oldItemPosition].id == newList[newItemPosition].id &&
+                oldList[oldItemPosition].title == newList[newItemPosition].title &&
+                oldList[oldItemPosition].description == newList[newItemPosition].description &&
+                oldList[oldItemPosition].priority == newList[newItemPosition].priority
     }
 }
