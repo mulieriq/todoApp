44428277155cfefa9545a776adb68b79f188f40e
EricRootLee <erickiokom@gmail.com>
2021-01-08 12:54:14 +0300

    Didd util impelementaion

--- app/src/main/java/com/skylabstechke/todo/utilis/ToDoDiffUtil.kt
+++ app/src/main/java/com/skylabstechke/todo/utilis/ToDoDiffUtil.kt
@@ -1,4 +1,25 @@
 package com.skylabstechke.todo.utilis
 
-class ToDoDiffUtil {
+import androidx.recyclerview.widget.DiffUtil
+import com.skylabstechke.todo.data.model.ToDoData
+
+class ToDoDiffUtil(
+    private val oldList: List<ToDoData>,
+    private val newList: List<ToDoData>
+) : DiffUtil.Callback() {
+    override fun getOldListSize(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun getNewListSize(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        TODO("Not yet implemented")
+    }
 }
