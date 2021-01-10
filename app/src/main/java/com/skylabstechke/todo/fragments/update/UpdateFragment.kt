6489ad00acc9603db78a5d9ebc9e8e4f0861155b
EricRootLee <erickiokom@gmail.com>
2021-01-11 00:31:19 +0300

    Fixed some bugs

--- app/src/main/java/com/skylabstechke/todo/fragments/update/UpdateFragment.kt
+++ app/src/main/java/com/skylabstechke/todo/fragments/update/UpdateFragment.kt
@@ -82,7 +82,7 @@ class UpdateFragment : Fragment() {
             )
             mToDoViewModel.updateData(updateData)
             Toast.makeText(requireContext(), "Successfully Updated!", Toast.LENGTH_LONG).show()
-            findNavController().navigate(R.id.action_updateFragment_to_listFragment)
+            findNavController().navigate(UpdateFragmentDirections.actionUpdateFragmentToDetailsFragment(updateData))
         } else {
             Toast.makeText(requireContext(), "Please fill out all fields", Toast.LENGTH_LONG).show()
         }
