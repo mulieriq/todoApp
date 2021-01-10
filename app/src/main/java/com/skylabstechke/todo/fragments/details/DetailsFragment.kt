e302671fa24f7b006e1cf5a36dd4bbd99010b88a
EricRootLee <erickiokom@gmail.com>
2021-01-10 23:16:17 +0300

    linked details to args

--- app/src/main/java/com/skylabstechke/todo/fragments/details/DetailsFragment.kt
+++ app/src/main/java/com/skylabstechke/todo/fragments/details/DetailsFragment.kt
@@ -5,16 +5,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
 import com.skylabstechke.todo.R
+import com.skylabstechke.todo.databinding.FragmentDetailsBinding
 
 class DetailsFragment : Fragment() {
+    private val args by navArgs<DetailsFragmentArgs>()
+    private var _binding FragmentDetailsBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
+        _binding = FragmentDetailsBinding.inflate(inflater,container,false)
+        binding.args = args
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_details, container, false)
+        return binding.root
+        inflater.inflate(R.layout.fragment_details, container, false)
     }
 
 }
