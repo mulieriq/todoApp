1035229b6527f6e0372bc1a87c5f46333591697e
EricRootLee <erickiokom@gmail.com>
2021-01-11 00:04:19 +0300

    Sending data to update

--- app/src/main/java/com/skylabstechke/todo/fragments/details/DetailsFragment.kt
+++ app/src/main/java/com/skylabstechke/todo/fragments/details/DetailsFragment.kt
@@ -1,28 +1,45 @@
 package com.skylabstechke.todo.fragments.details
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.skylabstechke.todo.R
 import com.skylabstechke.todo.databinding.FragmentDetailsBinding
 
 class DetailsFragment : Fragment() {
     private val args by navArgs<DetailsFragmentArgs>()
-    private var _binding:FragmentDetailsBinding? = null
+    private var _binding: FragmentDetailsBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentDetailsBinding.inflate(inflater,container,false)
+        _binding = FragmentDetailsBinding.inflate(inflater, container, false)
         binding.args = args
 
+        setHasOptionsMenu(true)
         return binding.root
 
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.update_item) {
+            findNavController().navigate(
+                DetailsFragmentDirections.actionDetailsFragmentToUpdateFragment2(
+                    args.current
+                )
+            )
+
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.details_fragment, menu)
     }
 
     override fun onDestroyView() {
