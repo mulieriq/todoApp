a98474cd17b53f5b9882c106196988609616628b
EricRootLee <erickiokom@gmail.com>
2021-01-11 14:17:47 +0300

    Added broad cast receiver

---
+++ app/src/main/java/com/skylabstechke/todo/app/ReminderBroadcast.kt
@@ -0,0 +1,24 @@
+package com.skylabstechke.todo.app
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.skylabstechke.todo.R
+
+class ReminderBroadcast : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent?) {
+
+        var builder = NotificationCompat.Builder(context, "notifyuser")
+            .setSmallIcon(R.drawable.ic_save)
+            .setContentTitle("Reminder")
+            .setContentText("changamka")
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+        NotificationManagerCompat.from(context).apply {
+            notify(200, builder.build())
+        }
+
+
+    }
+}
