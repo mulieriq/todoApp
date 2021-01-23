834448730e533f03dcdcdac722bfe57edcf6aad0
EricRootLee <erickiokom@gmail.com>
2021-01-24 00:24:59 +0300

    Removed action bar

--- app/src/main/java/com/skylabstechke/todo/fragments/onboarding/SplashScreen.kt
+++ app/src/main/java/com/skylabstechke/todo/fragments/onboarding/SplashScreen.kt
@@ -13,6 +13,7 @@ class SplashScreen : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_splash_screen, container, false)
     }
