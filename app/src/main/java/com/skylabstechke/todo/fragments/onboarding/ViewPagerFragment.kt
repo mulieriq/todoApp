a6bfc38201fb8c343ecc965f601ba5e0c334f845
EricRootLee <erickiokom@gmail.com>
2021-01-24 00:36:34 +0300

    WOrking on viewpager fragment

--- app/src/main/java/com/skylabstechke/todo/fragments/onboarding/ViewPagerFragment.kt
+++ app/src/main/java/com/skylabstechke/todo/fragments/onboarding/ViewPagerFragment.kt
@@ -6,6 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.skylabstechke.todo.R
+import com.skylabstechke.todo.fragments.onboarding.screens.SplashScreen1
+import com.skylabstechke.todo.fragments.onboarding.screens.SplashScreen2
+import com.skylabstechke.todo.fragments.onboarding.screens.SplashScreen3
 
 
 class ViewPagerFragment : Fragment() {
@@ -14,8 +17,15 @@ class ViewPagerFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val view = inflater.inflate(R.layout.fragment_view_pager, container, false)
 
-        return inflater.inflate(R.layout.fragment_view_pager, container, false)
+        val fragmentList = arrayListOf<Fragment>(
+            SplashScreen1(),
+            SplashScreen2(),
+            SplashScreen3()
+        )
+
+        return  view
     }
 
 
