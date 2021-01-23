573c3504fe042460d2773606939c926cfcd51ccd
EricRootLee <erickiokom@gmail.com>
2021-01-24 00:33:44 +0300

    done with pager

--- app/src/main/java/com/skylabstechke/todo/adapters/ViewPagerAdapter.kt
+++ app/src/main/java/com/skylabstechke/todo/adapters/ViewPagerAdapter.kt
@@ -5,12 +5,15 @@ import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Lifecycle
 import androidx.viewpager2.adapter.FragmentStateAdapter
 
-class ViewPagerAdapter(fm:FragmentManager,lifecycle:Lifecycle) :FragmentStateAdapter(fm,lifecycle) {
+class ViewPagerAdapter(
+    list:Array<Fragment>,
+    fm:FragmentManager,lifecycle:Lifecycle) :FragmentStateAdapter(fm,lifecycle) {
+    private val fragmentList=list;
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return fragmentList.size
     }
 
     override fun createFragment(position: Int): Fragment {
-        TODO("Not yet implemented")
+        return  fragmentList[position]
     }
 }
