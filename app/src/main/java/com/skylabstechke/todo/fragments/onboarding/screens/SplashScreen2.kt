38da5d16ff25908b260c09bb35e99857c5c519a3
EricRootLee <erickiokom@gmail.com>
2021-01-23 23:40:05 +0300

    Added all of the splash screens

---
+++ app/src/main/java/com/skylabstechke/todo/fragments/onboarding/screens/SplashScreen2.kt
@@ -0,0 +1,21 @@
+package com.skylabstechke.todo.fragments.onboarding.screens
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.skylabstechke.todo.R
+
+
+class SplashScreen2 : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_splash_screen2, container, false)
+    }
+
+}
---
+++ app/src/main/java/com/skylabstechke/todo/fragments/onboarding/screens/SplashScreen3.kt
@@ -0,0 +1,21 @@
+package com.skylabstechke.todo.fragments.onboarding.screens
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.skylabstechke.todo.R
+
+
+class SplashScreen3 : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_splash_screen3, container, false)
+    }
+
+}
