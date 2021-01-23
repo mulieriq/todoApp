4bd341b1371007994648b94cf133a4c73cafce8f
EricRootLee <erickiokom@gmail.com>
2021-01-24 00:27:44 +0300

    implementing virepagrs

--- app/src/main/java/com/skylabstechke/todo/adapters/ViewPagerAdapter.kt
+++ app/src/main/java/com/skylabstechke/todo/adapters/ViewPagerAdapter.kt
@@ -1,4 +1,16 @@
 package com.skylabstechke.todo.adapters
 
-class ViewPagerAdapter {
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class ViewPagerAdapter(fm:FragmentManager,lifecycle:Lifecycle) :FragmentStateAdapter(fm,lifecycle) {
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        TODO("Not yet implemented")
+    }
 }
